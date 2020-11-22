b766cec7842d837e626b2c76e52256b0891d513e
Dmitry Gorbachev <direon16@gmail.com>
2020-11-22 18:10:55 +0300

    Add files and settings for deployment on Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1!ro20a!7ez0c&hadg5fxa(auggk1)2q2q5n=de2!nvrryuo!k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://goldstorm.herokuapp.com/', 'https://gofinance.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com/', 'https://gofinance.herokuapp.com/']
 
 
 # Application definition
@@ -85,12 +85,28 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '<database_name>',
+        'USER': '<user_name>',
+        'PASSWORD': '<password>',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -144,3 +160,7 @@ SITE_ID = 1
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
+
+WHITENOISE_USE_FINDERS = True
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
